767a46991335b4f9fd4a374c329ffc5d04847784
uycoder <Thunder1987>
2023-01-30 20:32:22 +0300

    Change DB from MySQL to H2 databse. H2 databse with file based database.

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,85 +1,70 @@
-create table if not exists zhxy_db.tb_admin
-(
-    id            int auto_increment
-    primary key,
-    name          varchar(15)  null,
-    gender        char         null,
-    password      varchar(100) null,
-    email         varchar(50)  null,
-    telephone     varchar(12)  null,
-    address       varchar(100) null,
-    portrait_path varchar(200) null
-    )
-    charset = utf8mb3;
+CREATE TABLE IF NOT EXISTS tb_admin (
+                                                id INT AUTO_INCREMENT PRIMARY KEY,
+                                                name VARCHAR(15) NULL,
+    gender CHAR NULL,
+    password VARCHAR(100) NULL,
+    email VARCHAR(50) NULL,
+    telephone VARCHAR(12) NULL,
+    address VARCHAR(100) NULL,
+    portrait_path VARCHAR(200) NULL
+    ) ;
 
-create table if not exists zhxy_db.tb_clazz
-(
-    id            int auto_increment
-    primary key,
-    name          varchar(15)  null,
-    number        int          null,
-    introducation varchar(200) null,
-    headmaster    varchar(15)  null,
-    email         varchar(50)  null,
-    telephone     varchar(12)  null,
-    grade_name    varchar(15)  null
-    )
-    charset = utf8mb3;
+CREATE TABLE IF NOT EXISTS tb_clazz (
+                                                id INT AUTO_INCREMENT PRIMARY KEY,
+                                                name VARCHAR(15) NULL,
+    number INT NULL,
+    introducation VARCHAR(200) NULL,
+    headmaster VARCHAR(15) NULL,
+    email VARCHAR(50) NULL,
+    telephone VARCHAR(12) NULL,
+    grade_name VARCHAR(15) NULL
+    ) ;
 
-create table if not exists zhxy_db.tb_grade
-(
-    id            int auto_increment,
-    name          varchar(15) default '' not null,
-    manager       varchar(15)            null,
-    email         varchar(50)            null,
-    telephone     varchar(12)            null,
-    introducation varchar(200)           null,
-    primary key (id, name)
-    )
-    charset = utf8mb3;
+CREATE TABLE IF NOT EXISTS tb_grade (
+                                                id INT AUTO_INCREMENT,
+                                                name VARCHAR(15) DEFAULT '' NOT NULL,
+    manager VARCHAR(15) NULL,
+    email VARCHAR(50) NULL,
+    telephone VARCHAR(12) NULL,
+    introducation VARCHAR(200) NULL,
+    PRIMARY KEY (id, name)
+    ) ;
 
-create table if not exists zhxy_db.tb_student
-(
-    id            int auto_increment
-    primary key,
-    sno           varchar(20)  null,
-    name          varchar(15)  null,
-    gender        char         null,
-    password      varchar(100) null,
-    email         varchar(50)  null,
-    telephone     varchar(12)  null,
-    address       varchar(100) null,
-    introducation varchar(200) null,
-    portrait_path varchar(200) null,
-    clazz_name    varchar(15)  null
-    )
-    charset = utf8mb3;
+CREATE TABLE IF NOT EXISTS tb_student (
+                                                  id INT AUTO_INCREMENT PRIMARY KEY,
+                                                  sno VARCHAR(20) NULL,
+    name VARCHAR(15) NULL,
+    gender CHAR NULL,
+    password VARCHAR(100) NULL,
+    email VARCHAR(50) NULL,
+    telephone VARCHAR(12) NULL,
+    address VARCHAR(100) NULL,
+    introducation VARCHAR(200) NULL,
+    portrait_path VARCHAR(200) NULL,
+    clazz_name VARCHAR(15) NULL
+    ) ;
 
-create table if not exists zhxy_db.tb_teacher
-(
-    id            int auto_increment
-    primary key,
-    tno           varchar(20)  null,
-    name          varchar(15)  null,
-    gender        char         null,
-    password      varchar(100) null,
-    email         varchar(50)  null,
-    telephone     varchar(12)  null,
-    address       varchar(100) null,
-    portrait_path varchar(200) null,
-    clazz_name    varchar(15)  null
-    )
-    charset = utf8mb3;
+CREATE TABLE IF NOT EXISTS tb_teacher (
+                                                  id INT AUTO_INCREMENT PRIMARY KEY,
+                                                  tno VARCHAR(20) NULL,
+    name VARCHAR(15) NULL,
+    gender CHAR NULL,
+    password VARCHAR(100) NULL,
+    email VARCHAR(50) NULL,
+    telephone VARCHAR(12) NULL,
+    address VARCHAR(100) NULL,
+    portrait_path VARCHAR(200) NULL,
+    clazz_name VARCHAR(15) NULL
+    ) ;
 
-
-insert into MY_TABLE (id, name, gender, password, email, telephone, address, portrait_path)
+insert into tb_admin  (id, name, gender, password, email, telephone, address, portrait_path)
 values  (101, 'admin', 'F', '21232f297a57a5a743894a0e4a801fc3', '111111@gmail.com', '13260161111', 'Istanbul', 'upload/default.jpg'),
         (102, 'admin1', 'M', '21232f297a57a5a743894a0e4a801fc3', '512111559@gmail.com', '13260166090', 'Istanbul', 'upload/default.jpg'),
         (103, 'admin2', 'M', '21232f297a57a5a743894a0e4a801fc3', '512111559@gmail.com', '13260166091', 'Istanbul', 'upload/default.jpg'),
         (104, 'admin3', 'M', '21232f297a57a5a743894a0e4a801fc3', '123456@gmail.com', '13666666666', 'Ankara', 'upload/default.jpg');
 
 
-insert into MY_TABLE (id, name, number, introducation, headmaster, email, telephone, grade_name)
+insert into tb_clazz (id, name, number, introducation, headmaster, email, telephone, grade_name)
 values  (1, 'Class 1', 30, 'This is a test information', 'Ahmed', 'Ahmed@gmail.com', '13260166090', 'Grade 6'),
         (2, 'Class 2', 28, 'This is a test information', 'Abdushkur', 'Abdushkur@gmail.com', '13260166090', 'Grade 1'),
         (3, 'Class 3', 35, 'This is a test information', 'Ahmed', 'Ali@gmail.com', '13260166090', 'Grade 2'),
@@ -89,7 +74,7 @@ values  (1, 'Class 1', 30, 'This is a test information', 'Ahmed', 'Ahmed@gmail.c
         (7, 'Class 2', 30, 'This is a test information', 'Aburahman', 'Aburahman@gmail.com', '13260166090', 'Grade 4');
 
 
-insert into MY_TABLE (id, name, manager, email, telephone, introducation)
+insert into tb_grade (id, name, manager, email, telephone, introducation)
 values  (1, 'Grade 1', 'muhsin', 'muhsin@gmail.com', '13260166090', 'This is a test information'),
         (2, 'Grade 2', 'ali', 'ali@gmail.com', '13260166090', 'This is a test information'),
         (3, 'Grade 3', 'abdurahman', 'abdurahman@gmail.com', '13666666666', 'This is a test information'),
@@ -98,7 +83,7 @@ values  (1, 'Grade 1', 'muhsin', 'muhsin@gmail.com', '13260166090', 'This is a t
 
 
 
-insert into MY_TABLE (id, sno, name, gender, password, email, telephone, address, introducation, portrait_path, clazz_name)
+insert into tb_student (id, sno, name, gender, password, email, telephone, address, introducation, portrait_path, clazz_name)
 values  (1, '1001', 'Ahmed', 'M', 'e10adc3949ba59abbe56e057f20f883e', 'Ahmed@gmail.com', '13260166090', 'Ankara', 'This is a test information', 'upload/default.jpg', 'Class 1'),
         (2, '1002', 'Museb', 'M', 'e10adc3949ba59abbe56e057f20f883e', 'Museb@gmail.com', '13260166090', 'Ankara', 'This is a test information', 'upload/default.jpg', 'Class 2'),
         (3, '1003', 'Nefise', 'F', 'e10adc3949ba59abbe56e057f20f883e', 'Nefise@gmail.com', '13260166090', 'Ankara', 'This is a test information', 'upload/default.jpg', 'Class 1'),
@@ -109,7 +94,7 @@ values  (1, '1001', 'Ahmed', 'M', 'e10adc3949ba59abbe56e057f20f883e', 'Ahmed@gma
         (8, '1008', 'Yusuf', 'M', 'e10adc3949ba59abbe56e057f20f883e', 'Yusuf@gmail.com', '13260166090', 'Konya', 'This is a test information', 'upload/default.jpg', 'Class 2');
 
 
-insert into MY_TABLE (id, tno, name, gender, password, email, telephone, address, portrait_path, clazz_name)
+insert into tb_teacher (id, tno, name, gender, password, email, telephone, address, portrait_path, clazz_name)
 values  (1, '101', 'Meryem', 'F', '123456', 'dasheng@gmail.com', '13260166090', 'Meryem', 'upload/default.jpg', 'Class 3'),
         (2, '102', 'Abdulla', 'M', 'e10adc3949ba59abbe56e057f20f883e', 'Abdulla@gmail.com', '13260166090', 'Istanbul', 'upload/default.jpg', 'Class 2'),
         (3, '103', 'Ali', 'M', 'e10adc3949ba59abbe56e057f20f883e', 'Ali@gmail.com', '13260166090', 'Bursa', 'upload/default.jpg', 'Class 2'),
